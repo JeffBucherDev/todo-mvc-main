42037eb4546cf93497aefd5f1d53772af551c3ea
Jbucher89 <calmincolorado@gmail.com>
2021-10-06 11:31:47 -0600

    Finished Comments

--- models/Todo.js
+++ models/Todo.js
@@ -1,13 +1,13 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose') // stores mongoose in mongoose variable
 
-const TodoSchema = new mongoose.Schema({
-  todo: {
-    type: String,
-    required: true,
+const TodoSchema = new mongoose.Schema({ // Creates a new mongoose schema (structure of the todo documents)
+  todo: { // todo is the key
+    type: String, // type is a string
+    required: true, // forces todo to be a string
   },
-  completed: {
-    type: Boolean,
-    required: true,
+  completed: { // completed key
+    type: Boolean, // type is a boolean
+    required: true, // forces completed to be a boolean
   }
 })
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,69 +1,70 @@
-const deleteBtn = document.querySelectorAll('.del')
-const todoItem = document.querySelectorAll('span.not')
-const todoComplete = document.querySelectorAll('span.completed')
+// These all return Node Lists, not arrays
+const deleteBtn = document.querySelectorAll('.del') // targets everything that has a del class in the document
+const todoItem = document.querySelectorAll('span.not') // targets everything that has the span.not class in the document
+const todoComplete = document.querySelectorAll('span.completed') // targets everything that has the span.completed class in the document
 
-Array.from(deleteBtn).forEach((el)=>{
-    el.addEventListener('click', deleteTodo)
+Array.from(deleteBtn).forEach((el)=>{ // converts everything from the deleteBtn nodelist, and turns it into an array
+    el.addEventListener('click', deleteTodo) // adds an event listener that, upon clicks, executes the deleteTodo function
 })
 
-Array.from(todoItem).forEach((el)=>{
-    el.addEventListener('click', markComplete)
+Array.from(todoItem).forEach((el)=>{ // converts everything from the todoItem nodelist, and turns it into an array
+    el.addEventListener('click', markComplete) // adds an event listener that, upon clicking, executes the markComplete function
 })
 
-Array.from(todoComplete).forEach((el)=>{
-    el.addEventListener('click', markIncomplete)
+Array.from(todoComplete).forEach((el)=>{ // converts everything from the todoComplete nodelist, and turns it into an array
+    el.addEventListener('click', markIncomplete) // adds an event listener that, upon clicks, executes the markIncomplete function
 })
 
-async function deleteTodo(){
-    const todoId = this.parentNode.dataset.id
+async function deleteTodo(){  // asychronous function that deletes todo's
+    const todoId = this.parentNode.dataset.id // grabs and stores the parent nodes (li) ID from the database into todoId variable
     try{
-        const response = await fetch('todos/deleteTodo', {
-            method: 'delete',
-            headers: {'Content-type': 'application/json'},
-            body: JSON.stringify({
-                'todoIdFromJSFile': todoId
+        const response = await fetch('todos/deleteTodo', { // waits for the promise to resolve, then goes to the deletetodo url with the data
+            method: 'delete', // the type of request (delete)
+            headers: {'Content-type': 'application/json'}, // the type of content (json)
+            body: JSON.stringify({ // turns the following json object into a string
+                'todoIdFromJSFile': todoId // the key (todoIdFromJSFile) and its value (todoID (the id from the db))
             })
         })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+        const data = await response.json() // takes all the response from the above promise and stores it into data
+        console.log(data) // console logs that response
+        location.reload() // reloads the page
     }catch(err){
-        console.log(err)
+        console.log(err) // throws any errors into console
     }
 }
 
-async function markComplete(){
-    const todoId = this.parentNode.dataset.id
+async function markComplete(){ // asychronous function that marks todos complete
+    const todoId = this.parentNode.dataset.id // grabs and stores the parent nodes (li) id from the database into todoId
     try{
-        const response = await fetch('todos/markComplete', {
-            method: 'put',
-            headers: {'Content-type': 'application/json'},
-            body: JSON.stringify({
-                'todoIdFromJSFile': todoId
+        const response = await fetch('todos/markComplete', { // (promise) that waits for the resolution from the markComplete url data
+            method: 'put', // the type of request (put)
+            headers: {'Content-type': 'application/json'}, // the type of content (json)
+            body: JSON.stringify({ // turns the following json object into a string
+                'todoIdFromJSFile': todoId // the key (todoIdFromJSFile) and it's value (todoID (id from the db))
             })
         })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+        const data = await response.json() // takes all the response from the above promise and stores it into data variable
+        console.log(data) // logs data variable
+        location.reload() // reloads the page
     }catch(err){
-        console.log(err)
+        console.log(err) // console logs any error from the above code block
     }
 }
 
-async function markIncomplete(){
-    const todoId = this.parentNode.dataset.id
+async function markIncomplete(){  // asychronous function that marks  todos incomplete
+    const todoId = this.parentNode.dataset.id // grabs and stores the parent nodes (li) id from the database into todoId variable
     try{
-        const response = await fetch('todos/markIncomplete', {
-            method: 'put',
-            headers: {'Content-type': 'application/json'},
-            body: JSON.stringify({
-                'todoIdFromJSFile': todoId
+        const response = await fetch('todos/markIncomplete', { // waits for the promise to resolve from the markIncomplete url data
+            method: 'put', // the type of request (put)
+            headers: {'Content-type': 'application/json'}, // the type of content (json)
+            body: JSON.stringify({ // turns the following json object into a string
+                'todoIdFromJSFile': todoId // the key (todoIdFromJSFile) and its value (todoID (the id from the db))
             })
         })
-        const data = await response.json()
-        console.log(data)
-        location.reload()
+        const data = await response.json() // takes all the response from the above promise and stores int into data variable
+        console.log(data) // console logs the data variable
+        location.reload() // reloads the page
     }catch(err){
-        console.log(err)
+        console.log(err) // throws any errors into console
     }
 }
